281642fc764221e16faf44268dbe03e51bef387d
AraiNylaz <lorine.etienne@gmail.com>
2022-05-05 12:51:43 +0200

    fix merge conflict errors

--- config.h
+++ config.h
@@ -1,10 +1,21 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-#define SERVER_PORT 	80
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <sys/ipc.h>
+
 #define SERVER_IP		"127.0.0.1"  /* localhost */
 #define NBRCOMPTESENBANQUE 1000
-#define BACKLOG 5 //TODO vérifier si utile...
+#define BACKLOG 10
 #define FD_0 0
 #define FD_1 1
 #define FD_ERROR 2
@@ -12,15 +23,22 @@
 #define SHMKEY 1234
 #define SEMKEY 1234
 
+typedef struct ResponseClient{
+  int noCompteSource;
+  int montant;
+  int noCompteDestination;
+} ResponseClient;
 
-struct CompteEnBanque{
-  int solde;
-  int noCompte;
-} CompteEnBanque;
-struct Banque{
- int comptes[NBRCOMPTESENBANQUE];
-} Banque;
 
+typedef struct Transfer {
+	int type;// 0 --> virement récurent, 1 --> commande
+	ResponseClient response;
+} Transfer;
+
+typedef struct CompteEnBanque{
+int noCompte;
+int solde;
+}CompteEnBanque;
 
 #endif
 
