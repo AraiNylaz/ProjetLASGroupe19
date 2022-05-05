e46ed95354d30cf31525867086bf0aed6f32755e
unknown <bastien.verstappen@outlook.be>
2022-05-05 11:10:26 +0200

    fix: type  error

--- maint.c
+++ maint.c
@@ -13,7 +13,7 @@
 
 
 int main (int agrc , char* argv){
-    int type = argv[1] ;
+    int type = atoi(argv[1]) ;
     int shm_id;
     int sem_id;
     switch(type)
@@ -36,7 +36,7 @@ int main (int agrc , char* argv){
     //réserve la mémoire partagée
         printf("type=3");
         sem_down0(sem_id);
-        sleep(argv[2]);
+        sleep(atoi(argv[2]));
         sem_up0(sem_id);
         break;
     default:
