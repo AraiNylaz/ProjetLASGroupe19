fd004230a5ebd16f4c5ddcd45367eb566d97be21
unknown <bastien.verstappen@outlook.be>
2022-05-05 11:44:23 +0200

    feat pdr

--- pdr.c
+++ pdr.c
@@ -5,13 +5,22 @@
 #include "utils_v1.h"
 
 int main(int argc, char* argv){
-    int numCompte;
-    int montant;
+    int numCompte = atoi(argv[1]);
+    int montant=atoi(argv[2]);
+    int sem_id=sem_get(SEMKEY,1);
+    int shm_id=sshmget(SHMKEY,sizeof(int),PERM);
+    
+        
+    sem_down0(sem_id);
+    Banque* livreDeCompte = sshmat(shm_id);
     if (checkNeg(montant)){
-
+        //montant négatif => retrait 
+        *livreDeCompte[numCompte-1].solde-=montant;
     }else{
-
+        //montant positif => ajout
+        *livreDeCompte[numCompte-1].solde+=montant;
     }
-    return 0;
+    sem_up0(sem_id);
+    shmdt(solde);
 }
 
