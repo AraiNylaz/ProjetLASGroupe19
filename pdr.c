f5ceac2d635d222d6d06c97ee00ea06243e6faa7
unknown <bastien.verstappen@outlook.be>
2022-05-10 12:18:13 +0200

    refactor pdr

--- pdr.c
+++ pdr.c
@@ -7,16 +7,12 @@
 int main(int argc, char *argv[]){
     int numCompte = atoi(argv[1]);
     int montant= atoi(argv[2]);
-    printf("%d",montant);
     int sem_id=sem_get(SEMKEY,1);
     int shm_id=sshmget(SHMKEY,NBRCOMPTESENBANQUE*sizeof(int),0| PERM);
     struct Banque* livreDeCompte = sshmat(shm_id);
     sem_down0(sem_id);
-   
-   
+    //retrait ou depot 
     (*livreDeCompte).comptes[numCompte]+=montant;
-     printf("%d",livreDeCompte->comptes[numCompte]);
-    
     sem_up0(sem_id);
     sshmdt(livreDeCompte);
 
