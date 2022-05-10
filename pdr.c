b578bc385b52adb22bab0bd8fbe0f8aa6f75d76f
unknown <bastien.verstappen@outlook.be>
2022-05-10 11:31:41 +0200

    fin pdr

--- pdr.c
+++ pdr.c
@@ -4,22 +4,23 @@
 #include "config.h"
 #include "utils_v1.h"
 
-int main(int argc, char* argv){
-    int numCompte = argv[1];
-    int montant= argv[2];
+int main(int argc, char *argv[]){
+    int numCompte = atoi(argv[1]);
+    int montant= atoi(argv[2]);
     int sem_id=sem_get(SEMKEY,1);
-    int shm_id=sshmget(SHMKEY,sizeof(int),PERM);
-    
+    int shm_id=sshmget(SHMKEY,NBRCOMPTESENBANQUE*sizeof(int),0| PERM);
+    struct Banque* livreDeCompte = sshmat(shm_id);
     sem_down0(sem_id);
-    Banque* livreDeCompte = sshmat(shm_id);
-    if (checkNeg(montant)){
+    if (montant < 0){
         //montant négatif => retrait 
-        *livreDeCompte[numCompte-1].solde-=montant;
+        (*livreDeCompte).comptes[numCompte]-=montant;
+        //nread(1,livreDeCompte.comptes[numCompte-1],sizeof(int));
     }else{
         //montant positif => ajout
-        *livreDeCompte[numCompte-1].solde+=montant;
+        (*livreDeCompte).comptes[numCompte]+=montant;
     }
     sem_up0(sem_id);
-    shmdt(solde);
+    sshmdt(livreDeCompte);
+
 }
 
